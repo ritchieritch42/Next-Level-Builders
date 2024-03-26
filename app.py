51a85879d6374ffb5f273c187811009ba1f299ff
Dr Giggle Touch <ritchie.caleb42@gmail.com>
2024-03-26 18:29:13 -0500

    template error message change and recaptcha function readded

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-from flask import Flask, render_template, request
-from helpers import send_email
+from flask import Flask, flash, render_template, request
+from helpers import send_email, is_recaptcha_valid
 from dotenv import load_dotenv
+import os
 
 #Function loads key value pairs from the .env (non-syndicated environment file)
 load_dotenv()
 
 # Configure application
 app = Flask(__name__)
+
+# Set a secret key for Flask sessions
+app.secret_key = os.urandom(24).hex()
 
 # Define a get route for the home page of the website
 @app.route("/")
@@ -22,24 +26,35 @@ def about():
 def contact():
     # If the user is submitting a contact form, then do the following...
     if request.method == "POST":
-        # Define the users receiving email, subject, and body
-        subject = request.form.get("subject")
-        firstname = request.form.get("firstname")
-        lastname = request.form.get("lastname")
-        phonenumber = request.form.get("phonenumber")
-        email = request.form.get("email")
-        emailbody = request.form.get("body")
-        body = firstname + " " + lastname + "\n" + phonenumber + "\n" + email + "\n" + emailbody
 
-        # Attempt to send the contact submission form details
-        # Email will always be the contact email
-        # Subject is subject
-        # Body is firstname, lastname, phonenumber, email, and the content
-        try:
-            send_email(subject, body)
-            return render_template("index.html")
-        except:
-            return render_template("sendemailfailure.html")
+    # Get the reCAPTCHA response from the form
+        recaptcha_response = request.form.get("g-recaptcha-response")
+        
+        # Check if the reCAPTCHA response is valid
+        if not is_recaptcha_valid(recaptcha_response):
+            flash("Please ensure that you are human via reCAPTCHA")
+            return render_template("contact.html")
+
+        else:
+            # Define the users receiving email, subject, and body
+            subject = request.form.get("subject")
+            firstname = request.form.get("firstname")
+            lastname = request.form.get("lastname")
+            phonenumber = request.form.get("phonenumber")
+            email = request.form.get("email")
+            emailbody = request.form.get("body")
+            body = firstname + " " + lastname + "\n" + phonenumber + "\n" + email + "\n" + emailbody
+
+            # Attempt to send the contact submission form details
+            # Email will always be the contact email
+            # Subject is subject
+            # Body is firstname, lastname, phonenumber, email, and the content
+            try:
+                send_email(subject, body)
+                flash("Your message was sent successfully")
+                return render_template("index.html")
+            except:
+                return render_template("sendemailfailure.html")
 
     # If the user hasn't submitted a contact form yet, then load the contact page
     else:
--- helpers.py
+++ helpers.py
@@ -1,14 +1,20 @@
+# For send_email
 import smtplib, os
+import requests
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
 def send_email(subject, body):
+    # Define server variables
     smtp_server = "smtp.gmail.com"
     smtp_port = 587
+
+    # Define emails and passwords
     sender_email = os.getenv("SENDER_EMAIL")
     receiver_email = os.getenv("RECEIVER_EMAIL")
     password = os.getenv("PASSWORD")
 
+    # Define message contents
     message = MIMEMultipart()
     message["From"] = sender_email
     message["To"] = receiver_email
@@ -27,3 +33,18 @@ def send_email(subject, body):
 
     # Close the SMTP session
     server.quit()
+
+def verify_recaptcha(response):
+    payload = {
+        'secret': os.getenv("RECAPTCHA_SECRET_KEY"),
+        'response': response
+    }
+    response = requests.post('https://www.google.com/recaptcha/api/siteverify', data=payload)
+    data = response.json()
+    return data['success']
+
+# Check if the reCAPTCHA response is valid
+def is_recaptcha_valid(response):
+    if not response:
+        return False
+    return verify_recaptcha(response)
