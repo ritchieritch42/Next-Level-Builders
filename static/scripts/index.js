3733ffb222a0277addf99ba74c71fbdb425ea494
Dr Giggle Touch <ritchie.caleb42@gmail.com>
2024-03-30 14:08:18 -0500

    explicity declared navbar children classes to change text color, nextSibling method is not working

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -8,11 +8,22 @@ function navColorChange(elementClass) {
   window.addEventListener("scroll", () => {
     y = window.scrollY;
     let navbar = document.querySelector(elementClass);
+    let links = document.querySelectorAll(".navbarlink");
+    console.log(links);
+    let businessName = document.querySelector(".businessname");
 
     if (y > 100) {
       navbar.style.backgroundColor = "#38393b";
+      for (i = 0; i < 3; i++) {
+        links[i].style.color = "white";
+      }
+      businessName.style.color = "white";
     } else {
       navbar.style.backgroundColor = "white";
+      for (i = 0; i < 3; i++) {
+        links[i].style.color = "#38393b";
+      }
+      businessName.style.color = "#38393b";
     }
   });
 }
