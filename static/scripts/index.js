f698087ecb75f7fbce23a5e98a9dd0fb72a537d2
Dr Giggle Touch <ritchie.caleb42@gmail.com>
2024-03-30 11:26:21 -0500

    edited scroll function to have text appear sooner, and not based off previous element

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -10,16 +10,13 @@ function scroll(elementClass) {
     let textLocation = text.getBoundingClientRect();
     let y = textLocation.y;
 
-    if (y < 500 && y > 0) {
-      // Calculate new opacity value based on scroll position
-      let newOpacity = Math.min(1, Math.max(0, 1 - Math.abs(y) / 500));
-      // Set the opacity
-      text.style.opacity = newOpacity; // Corrected to `text` instead of `elementClass`
-    } else if (y < 0) {
-      // Calculate new opacity value based on scroll position
-      text.style.opacity = 1; // Corrected to `text` instead of `elementClass`
+    if (y > 1000) {
+      text.style.opacity = "0.25";
+    } else if (y < 1000 && y > 400) {
+      newOpacity = (1000 - y) / 1000;
+      text.style.opacity = newOpacity;
     } else {
-      text.style.opacity = 0; // Corrected to `text` instead of `elementClass`
+      text.style.opacity = "1";
     }
   });
 }
