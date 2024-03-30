e247628f752179a207b769f0836266343f5bf543
Dr Giggle Touch <ritchie.caleb42@gmail.com>
2024-03-30 14:19:04 -0500

    consolidated back to index.js for now

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // scroll(".featuredparagraph");
-  // scroll(".featuredprojectheader");
+  scroll(".featuredparagraph");
+  scroll(".featuredprojectheader");
   navColorChange(".navbar");
 });
 
@@ -11,6 +11,7 @@ function navColorChange(elementClass) {
     let links = document.querySelectorAll(".navbarlink");
     console.log(links);
     let businessName = document.querySelector(".businessname");
+    let logo = document.querySelector(".logo");
 
     if (y > 100) {
       navbar.style.backgroundColor = "#38393b";
@@ -18,12 +19,33 @@ function navColorChange(elementClass) {
         links[i].style.color = "white";
       }
       businessName.style.color = "white";
+      businessName.style.fontSize = "3rem";
+      logo.style.opacity = "0";
     } else {
       navbar.style.backgroundColor = "white";
       for (i = 0; i < 3; i++) {
         links[i].style.color = "#38393b";
       }
       businessName.style.color = "#38393b";
+      businessName.style.fontSize = "2.66rem";
+      logo.style.opacity = "1";
+    }
+  });
+}
+
+function scroll(elementClass) {
+  window.addEventListener("scroll", () => {
+    let text = document.querySelector(elementClass);
+    let textLocation = text.getBoundingClientRect();
+    let yFeaturedParagraph = textLocation.y;
+
+    if (yFeaturedParagraph > 1000) {
+      text.style.opacity = "0.25";
+    } else if (yFeaturedParagraph < 1000 && yFeaturedParagraph > 400) {
+      newOpacity = (1000 - yFeaturedParagraph) / 1000;
+      text.style.opacity = newOpacity;
+    } else {
+      text.style.opacity = "1";
     }
   });
 }
