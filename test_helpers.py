8936c9f549e526d478ae632fc65bd5bf3f0b724c
Dr Giggle Touch <ritchie.caleb42@gmail.com>
2024-05-22 11:40:22 -0500

    s3 get_object test created in full

--- test_helpers.py
+++ test_helpers.py
@@ -1,10 +1,9 @@
-import unittest, boto3, os
+import unittest, boto3, json
 from moto import mock_aws
 from helpers import fetch_parameters
 from dotenv import dotenv_values
 
 class TestParameterGet(unittest.TestCase):
-
     @mock_aws
     def test_parameter_get(self):
         client = boto3.client('ssm')
@@ -28,6 +27,43 @@ class TestParameterGet(unittest.TestCase):
         print(parameters)
 
         self.assertEqual(env_parameters['contactnextlevelbuilders_email'], parameters['contactnextlevelbuilders_email'])
+
+    if __name__ == '__main__':
+        unittest.main()
+
+class TestS3ContentGet(unittest.TestCase):
+    # Mock AWS Environment
+    @mock_aws
+    def test_s3_get(self):
+        # Create a client connection for s3 to test with
+        client = boto3.client('s3')
+        # Create a test bucket to put JSON data into
+        client.create_bucket(
+            Bucket='test'
+        )
+
+        # Open file object, then load this as JSON into variable s3object
+        with open('nextlevelbuilder-1711565908511-5387e3f09914.json', 'r') as file:
+            s3object = json.load(file)
+
+        # Put the s3object into the test bucket
+        client.put_object(
+            Body=json.dumps(s3object),
+            Bucket='test',
+            Key='test'
+        )
+
+        # Retrieve the object from S3 test bucket
+        response = client.get_object(Bucket='test', Key='test')
+        # Decode the response using UTF-8 standard
+        content = response['Body'].read().decode('utf-8')
+        # Load as dictionary to reference
+        json_content = json.loads(content)
+
+        # Print the content of the return get_object
+        print(json_content)
+        # Test that it's equal to put_object
+        self.assertEqual(s3object, json_content)
 
     if __name__ == '__main__':
         unittest.main()
