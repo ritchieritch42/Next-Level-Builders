0e0350dfe4b607d810cbb32567bd7106ed774ab4
Dr Giggle Touch <ritchie.caleb42@gmail.com>
2024-06-18 07:24:59 -0500

    removed recaptcha

--- app.py
+++ app.py
@@ -22,22 +22,8 @@ def about():
 def contact():
     # If the user is submitting a contact form, then do the following...
     if request.method == "POST":
-
-        # Get the token from the google recaptcha
-        token = request.form.get('g-recaptcha-response')
-
-        # Get parameters from helpers.py
-        prefix = 'contactnextlevelbuilders_'
-        parameters = fetch_parameters(prefix)
-
-        # Check if reCAPTCHA token is empty
-        if not token:
-            flash("Please complete the reCAPTCHA")
-            return render_template("contact.html")
-
-        # Verify reCAPTCHA
-        if verify_recaptcha(token, parameters):
-            # reCAPTCHA verification passed, proceed with sending form content
+        try: 
+            # Proceed with sending form content
             subject = request.form.get("subject")
             firstname = request.form.get("firstname")
             lastname = request.form.get("lastname")
@@ -53,11 +39,8 @@ def contact():
             send_email(subject, body)
             flash("Your contact form was submitted.")
             return render_template("index.html")
-
-        else:
-            # reCAPTCHA verification failed, return error response
-            flash("")
-            return jsonify({'success': False, 'message': 'reCAPTCHA verification failed'})
+        except:
+            return render_template("sendemailfailure.html")
 
     # If the user hasn't submitted a contact form yet, then load the contact page
     else:
