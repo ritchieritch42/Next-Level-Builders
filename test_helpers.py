a6a7d54046786496ebf76b2e26aa906c0e1f7763
Caleb <ritchie.caleb42@gmail.com>
2024-09-23 12:31:43 -0500

    comment out email test for now

--- test_helpers.py
+++ test_helpers.py
@@ -1,6 +1,7 @@
 import unittest, boto3
+from unittest.mock import patch, MagicMock
 from moto import mock_aws
-from helpers import fetch_parameters
+from helpers import fetch_parameters, send_email
 from dotenv import dotenv_values
 
 class TestHelpers(unittest.TestCase):
@@ -10,43 +11,58 @@ class TestHelpers(unittest.TestCase):
         env_parameters = {**dotenv_values(".env")}
         keysList = list(env_parameters.keys())
         valuesList = list(env_parameters.values())
+
+        print(env_parameters)
+        print(keysList)
+        print(valuesList)
         
         i = 0
         length_env = len(keysList)
+        print(length_env)
 
         while i < length_env:
             client.put_parameter(
                 Name=keysList[i],
                 Value=valuesList[i],
-                Type='SecureString')
+                Type='String')
             i += 1
-         
+
         prefix = 'contactnextlevelbuilders_'
         parameters = fetch_parameters(prefix)
 
-        print('SUCCESS: ', parameters)
-
         self.assertEqual('contactnextlevelbuilders@gmail.com', parameters['contactnextlevelbuilders_email'])
-
-    def test_send_email(self):
-        # Reset self or any other reused variables
-        # Setup
-        parameters = {
-            contactnextlevelbuilders_email: 'sendemail@email.com',
-            contactnextlevelbuilders_receiver-email: 'receiveremail@email.com',
-            contactnextlevelbuilders_password: 'testpassword'
-        }
-
-        subject = 'Test Email'
-        body = 'Test Body'
-
-        # Run
-        result = send_email(subject, body, parameters)
-
-        # Assert
-        # Assert SMTP server is started
-        # Email is sent through server
-        # Session with server is closed
 
 if __name__ == '__main__':
     unittest.main()
+
+    # @patch('helpers.smtplib.SMTP', autospec=True)
+    # def test_send_email(self):
+    #     # Setup
+    #     parameters = {
+    #         'contactnextlevelbuilders_email': 'sendemail@email.com',
+    #         'contactnextlevelbuilders_receiver-email': 'receiveremail@email.com',
+    #         'contactnextlevelbuilders_password': 'testpassword'
+    #     }
+
+    #     subject = 'Test Email'
+    #     body = 'Test Body'
+
+    #     # Run
+    #     result = send_email(subject, body, parameters)
+
+    #     # Assert
+    #     # Check if the SMTP session was started with the correct server and port
+    #     mock_smtp.assert_called_once_with("smtp.gmail.com", 587)
+
+    #     # Check if starttls was called to start TLS encryption
+    #     mock_server.starttls.assert_called_once()
+
+    #     # Check if login was called with the correct email and password
+    #     mock_server.login.assert_called_once_with('sendemail@email.com', 'testpassword')
+
+    #     # Check if sendmail was called with the correct parameters
+    #     mock_server.sendmail.assert_called_once_with(
+    #         'sendemail@email.com',
+    #         'receiveremail@email.com',
+    #         mock_server.sendmail.call_args[0][2]  # The third argument is the email message string
+    #     )
