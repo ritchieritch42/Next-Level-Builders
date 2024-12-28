b1274508d0d30cd175a64403f0df311dd940caeb
ritchieritch42 <ritchie.caleb42@gmail.com>
2024-12-27 19:45:11 -0600

    Boxes for home page

--- serverless/frontend-s3/src/Home.jsx
+++ serverless/frontend-s3/src/Home.jsx
@@ -1,12 +1,17 @@
-import { Box, Typography } from '@mui/material';
+import { Box, Typography } from "@mui/material";
 import "./Home.css";
 
 function Home() {
-    return (
-        <Box className="homepage">
-            <Typography> This is the home page </Typography>
-        </Box>
-    );
+  return (
+    <Box className="homepage">
+      <Box></Box>
+      <Box></Box>
+      <Box></Box>
+      <Box></Box>
+      <Box></Box>
+      <Box></Box>
+    </Box>
+  );
 }
 
 export default Home;
