25f0f94a7bba1342cab90da5a6d101dacf98d8fa
ritchieritch42 <ritchie.caleb42@gmail.com>
2024-12-28 13:10:20 -0600

    homepage introduction check

--- serverless/frontend-s3/src/Home.jsx
+++ serverless/frontend-s3/src/Home.jsx
@@ -4,12 +4,17 @@ import "./Home.css";
 function Home() {
   return (
     <Box className="homepage">
-      <Box></Box>
-      <Box></Box>
-      <Box></Box>
-      <Box></Box>
-      <Box></Box>
-      <Box></Box>
+      <Box className="homepage-introduction">
+        <Box
+          alt="A picture of Union Station and the Downtown Kansas City Area"
+          className="homepage-introduction-image"
+          component="img"
+          src="/kc-andrew-peterson.jpg"
+        />
+        <Typography className="homepage-introduction-header" variant="h4">
+          Providing cost-effective construction solutions with integrity
+        </Typography>
+      </Box>
     </Box>
   );
 }
