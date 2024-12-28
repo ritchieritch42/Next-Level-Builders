01643464dec8890e7a5b92f478c3cfa6dcafd70e
ritchieritch42 <ritchie.caleb42@gmail.com>
2024-12-27 19:42:35 -0600

    font to monserrat

--- serverless/frontend-s3/src/App.js
+++ serverless/frontend-s3/src/App.js
@@ -5,6 +5,7 @@ import Footer from "./Footer";
 import Home from "./Home";
 import Navbar from "./Navbar";
 import { Routes, Route } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
