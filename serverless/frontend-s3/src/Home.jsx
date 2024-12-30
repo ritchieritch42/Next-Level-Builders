ce2c468c405d10990f92b319cf7a6427c27483eb
ritchieritch42 <ritchie.caleb42@gmail.com>
2024-12-30 15:16:34 -0600

    homepage-services content

--- serverless/frontend-s3/src/Home.jsx
+++ serverless/frontend-s3/src/Home.jsx
@@ -22,6 +22,45 @@ function Home() {
           </Box>
         </Box>
       </Box>
+      <Box className="homepage-services">
+        <Box className="homepage-services-header">
+          <Typography className="homepage-services-header-text" variant="h2">
+            Builds Include
+          </Typography>
+        </Box>
+        <Box className="homepage-services-types">
+          <Box className="homepage-services-types-commercial">
+            <Box
+              className="homepage-services-types-commercial-image"
+              component="img"
+              src="/commercial1x1.png"
+            />
+            <Typography className="homepage-services-type-commercial-text">
+              Commercial
+            </Typography>
+          </Box>
+          <Box className="homepage-services-types-industrial">
+            <Box
+              className="homepage-services-types-industrial-image"
+              component="img"
+              src="/industrial.jpg"
+            />
+            <Typography className="homepage-services-types-industrial-text">
+              Light Industrial
+            </Typography>
+          </Box>
+          <Box className="homepage-services-types-tenantfinish">
+            <Box
+              className="homepage-services-types-tenantfinish-image"
+              component="img"
+              src="/tenantfinish.jpg"
+            />
+            <Typography className="homepage-services-types-tenantfinish-text">
+              Tenant Finish
+            </Typography>
+          </Box>
+        </Box>
+      </Box>
     </Box>
   );
 }
