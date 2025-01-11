84dca4fb2121b6667155207082c227397435a204
Caleb <ritchie.caleb42@gmail.com>
2025-01-11 12:02:16 -0600

    featured project photos

--- serverless/frontend-s3/src/Home.jsx
+++ serverless/frontend-s3/src/Home.jsx
@@ -1,5 +1,44 @@
-import { Box, Typography } from "@mui/material";
+import { Box, ImageList, ImageListItem, Typography } from "@mui/material";
 import "./Home.css";
+
+const itemData = [
+  {
+    img: 'BVD Phase 1 Reception Desk.jpg',
+    title: 'Blue Valley Dermatology Phase 1 Reception Desk',
+  },
+  {
+    img: 'admin1.JPG',
+    title: 'Administrative Desk',
+  },
+  {
+    img: 'admin2.JPG',
+    title: 'Administrative Desk',
+  },
+  {
+    img: 'rcptn1.JPG',
+    title: 'Reception Area',
+  },
+  {
+    img: 'rcptn2.JPG',
+    title: 'Reception Area',
+  },
+  {
+    img: 'ktchn3.JPG',
+    title: 'Kitchen',
+  },
+  {
+    img: 'ktchn2.JPG',
+    title: 'Kitchen',
+  },
+  {
+    img: 'br1.JPG',
+    title: 'Bathroom',
+  },
+  {
+    img: 'office1.JPG',
+    title: 'Office',
+  },
+];
 
 function Home() {
   return (
@@ -62,9 +101,30 @@ function Home() {
         </Box>
       </Box>
       <Box className="homepage-featuredproject">
-        <Typography className="homepage-featuredproject-text" variant="h3">
-          Featured Project
+        <Box className="homepage-featuredproject-header">
+          <Typography className="homepage-featuredproject-header-text" variant="h3">
+            Featured Project
+          </Typography>
+        </Box>
+        <Typography className="homepage-featuredproject-description">
+          Renovation of existing office space, adding 5,000 square feet of medical
+          clinic to the Blue Valley Dermatology facility. The newly finished area
+          includes Moh's surgery suites, Moh's and Pathology Labs, offices, exam rooms
+          and reception areas. The work included demolition, carpentry, finishes,
+          casework, fire sprinkler, plumbing, HVAC, electrical and fire alarm.
         </Typography>
+        <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
+          {itemData.map((item) => (
+            <ImageListItem key={item.img}>
+              <img
+                srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+                alt={item.title}
+                loading="lazy"
+              />
+            </ImageListItem>
+          ))}
+        </ImageList>
       </Box>
     </Box>
   );
