dd909042774a8e9a3922c0e9ea72f303eebac318
Caleb <ritchie.caleb42@gmail.com>
2025-01-13 15:16:42 -0600

    about page content up

--- serverless/frontend-s3/src/About.jsx
+++ serverless/frontend-s3/src/About.jsx
@@ -1,9 +1,35 @@
-import { Box, Typography } from '@mui/material';
+import { Box, ImageList, ImageListItem, Typography } from '@mui/material';
 import './About.css';
+
+const itemData = [
+    {
+        img: 'craig.jpg',
+        title: 'Headshot of Craig Ritchie'
+    },
+    {
+        img: 'ulm-academic-logo-circle.png',
+        title: 'University of Louisiana Monroe Logo'
+    },
+]
 
 function About() {
     return (
         <Box className='aboutpage'>
+            <Box className='aboutpage-images'>
+                <ImageList sx={{ width: 450, height: 250 }} cols={2} rowHeight={175}>
+                {itemData.map((item) => (
+                    <ImageListItem className='aboutpage-imagelistitem' key={item.img}>
+                    <img
+                        alt={item.title}
+                        className='aboutpage-image'
+                        loading="lazy"
+                        srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                        src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+                    />
+                    </ImageListItem>
+                ))}
+                </ImageList>
+            </Box>
             <Box className='aboutpage-text'>
                 <Typography variant='h4'>Craig Ritchie, Owner || 30+ Years in The Industry</Typography>
                 <Typography variant='body1'>Craig has worked in the construction industry since graduating with a
