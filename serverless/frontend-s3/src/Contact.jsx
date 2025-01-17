9d1e9da86b19580c7da3c0ae823971f31928bfe4
ritchieritch42 <ritchie.caleb42@gmail.com>
2025-01-16 19:55:10 -0600

    submission button

--- serverless/frontend-s3/src/Contact.jsx
+++ serverless/frontend-s3/src/Contact.jsx
@@ -1,24 +1,34 @@
-import { Box, TextField, Typography } from '@mui/material';
-import './Contact.css';
+import { Box, Button, TextField, Typography } from "@mui/material";
+import SendOutlinedIcon from "@mui/icons-material/SendOutlined";
+import "./Contact.css";
 
 function Contact() {
-    return (
-        <Box className='contactpage'>
-            <Typography variant='h5'>Contact Next Level Builders, LLC</Typography>
-            <Typography variant='body1'>
-                Please fill out the contact 
-                form below, click send, and we will get back with you shortly 
-            </Typography>
-            <Box className='contactpage-fields'>
-                <TextField id="filled-basic" label="First Name" variant="filled" />
-                <TextField id="filled-basic" label="Last Name" variant="filled" />
-                <TextField id="filled-basic" label="Email" variant="filled" />
-                <TextField id="filled-basic" label="Phone Number" variant="filled" />
-                <TextField id="filled-basic" label="Subject" variant="filled" />
-                <TextField id="filled-basic" label="Body" multiline variant="filled" rows={8}/>
-            </Box>
-        </Box>
-    );
+  return (
+    <Box className="contactpage">
+      <Typography variant="h5">Contact Next Level Builders, LLC</Typography>
+      <Typography variant="body1">
+        Please fill out the contact form below, click send, and we will get back
+        with you shortly
+      </Typography>
+      <Box className="contactpage-fields">
+        <TextField id="filled-basic" label="First Name" variant="filled" />
+        <TextField id="filled-basic" label="Last Name" variant="filled" />
+        <TextField id="filled-basic" label="Email" variant="filled" />
+        <TextField id="filled-basic" label="Phone Number" variant="filled" />
+        <TextField id="filled-basic" label="Subject" variant="filled" />
+        <TextField
+          id="filled-basic"
+          label="Body"
+          multiline
+          variant="filled"
+          rows={8}
+        />
+        <Button variant="contained" endIcon={<SendOutlinedIcon />}>
+          Send
+        </Button>
+      </Box>
+    </Box>
+  );
 }
 
 export default Contact;
