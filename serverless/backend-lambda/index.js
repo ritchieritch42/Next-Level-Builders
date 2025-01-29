f503fb1e86ab780a77f14188d9cd72978091a50d
ritchieritch42 <ritchie.caleb42@gmail.com>
2025-01-29 17:49:34 -0600

    apigateway event handler

--- serverless/backend-lambda/index.js
+++ serverless/backend-lambda/index.js
@@ -1,28 +1,30 @@
-import { S3Client, HeadObjectCommand } from "@aws-sdk/client-s3";
+const headers = {
+  "Access-Control-Allow-Origin": "*",
+  "Content-Type": "application/json",
+};
 
-const client = new S3Client();
+export const handler = async (event) => {
+  const body =
+    typeof event.body === "string" ? JSON.parse(event.body) : event.body;
+  // define path string to check for product_id in updateProduct
+  const path = event.path;
+  const httpMethod = event.httpMethod;
+  let response;
 
-export const handler = async (event, context) => {
-  // Get the object from the event and show its content type
-  const bucket = event.Records[0].s3.bucket.name;
-  const key = decodeURIComponent(
-    event.Records[0].s3.object.key.replace(/\+/g, " ")
-  );
-
-  try {
-    const { ContentType } = await client.send(
-      new HeadObjectCommand({
-        Bucket: bucket,
-        Key: key,
-      })
-    );
-
-    console.log("CONTENT TYPE:", ContentType);
-    return ContentType;
-  } catch (err) {
-    console.log(err);
-    const message = `Error getting object ${key} from bucket ${bucket}. Make sure they exist and your bucket is in the same region as this function.`;
-    console.log(message);
-    throw new Error(message);
+  if (path === "/Contact") {
+    // Attempt to send form contents
+    if (httpMethod === "POST") {
+      response = await formService.sendForm(body);
+    }
   }
+
+  if (response === undefined) {
+    throw new Error(`No response`);
+  }
+
+  return {
+    headers,
+    statusCode: 200,
+    body: typeof response === "string" ? response : JSON.stringify(response),
+  };
 };
