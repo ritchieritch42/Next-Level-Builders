e8efb9373bc2834ba7daa878fcbee6531afcd98a
ritchieritch42 <ritchie.caleb42@gmail.com>
2025-01-30 14:48:30 -0600

    placeholder for sendForm

--- serverless/backend-lambda/formService.js
+++ serverless/backend-lambda/formService.js
@@ -1,6 +1,6 @@
-import validation from "./captcha.js";
-
 const sendForm = async (body, parameters) => {
   // Use SNS from AWS instead of JS SMTP library
+  console.log(body);
+  console.log(parameters);
   return;
 };
