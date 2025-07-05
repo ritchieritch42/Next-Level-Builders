17d91553e4ef9c1e36f2b2e449407b993762ca9a
Caleb <ritchie.caleb42@gmail.com>
2025-07-05 13:06:34 -0500

    Routing: /, /about, /projects

--- react vite/next-level-builders/src/App.jsx
+++ react vite/next-level-builders/src/App.jsx
@@ -1,13 +1,18 @@
 import About from "./pages/About";
 import Homepage from "./pages/Homepage";
 import Projects from "./pages/Projects";
+import { BrowserRouter, Routes, Route } from "react-router";
 
 function App() {
   return (
     <>
-      <Homepage />
-      <About />
-      <Projects />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
