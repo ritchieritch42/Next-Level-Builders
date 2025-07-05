4ed4da07bf72b7868866c059707a71822759faed
Caleb <ritchie.caleb42@gmail.com>
2025-07-05 13:10:52 -0500

    Routing: * for pagenotfound

--- react vite/next-level-builders/src/App.jsx
+++ react vite/next-level-builders/src/App.jsx
@@ -1,5 +1,6 @@
 import About from "./pages/About";
 import Homepage from "./pages/Homepage";
+import PageNotFound from "./pages/PageNotFound";
 import Projects from "./pages/Projects";
 import { BrowserRouter, Routes, Route } from "react-router";
 
@@ -11,6 +12,7 @@ function App() {
           <Route path="/" element={<Homepage />} />
           <Route path="/about" element={<About />} />
           <Route path="/projects" element={<Projects />} />
+          <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
     </>
