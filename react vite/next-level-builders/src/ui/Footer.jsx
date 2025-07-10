09b2d7eda0bf9a69d748610adea0b4387346d90a
Caleb <ritchie.caleb42@gmail.com>
2025-07-10 07:11:14 -0500

    Homepage: footer

--- react vite/next-level-builders/src/ui/Footer.jsx
+++ react vite/next-level-builders/src/ui/Footer.jsx
@@ -1,13 +1,19 @@
 import Box from "./Box";
 import Card from "./Card";
+import Text from "./Text";
+import { SiNetlify } from "react-icons/si";
 
 function Footer() {
   return (
-    <Card bgcolor="var(--color-red-400)">
-      <Box placement="center">
-        <p>Hello World!</p>
-      </Box>
-    </Card>
+    <Box placement="center">
+      <Text color="var(--color-tan-100)">
+        © 2025 Next Level Builders, LLC. All Rights Reserved.
+      </Text>
+      <Text color="var(--color-tan-100)">Hosted on Netlify</Text>
+      <a href="https://www.netlify.com/" target="_blank">
+        <SiNetlify size={30} style={{ fill: "var(--color-tan-100)" }} />
+      </a>
+    </Box>
   );
 }
 
