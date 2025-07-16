12116827a9d3f02727f6f9ac89d529405fb7f4eb
Caleb <ritchie.caleb42@gmail.com>
2025-07-16 16:47:11 -0500

    Sidebar: Dropdown with links

--- react vite/next-level-builders/src/ui/Link.jsx
+++ react vite/next-level-builders/src/ui/Link.jsx
@@ -2,8 +2,7 @@ import { NavLink } from "react-router";
 import styled from "styled-components";
 
 const StyledLink = styled(NavLink)`
-  color: var(--color-tan-100);
-  background-color: var(--color-red-500);
+  color: #000000;
   font-size: 1.3rem;
   font-weight: 400;
   text-decoration: none;
@@ -11,18 +10,18 @@ const StyledLink = styled(NavLink)`
   border-radius: 10px;
   transition: 0.5s;
 
-  &.active {
+  /* &.active {
     box-shadow: ${(props) =>
-      props.children === "About"
-        ? "inset 120px 0 0 0 var(--color-tan-200)" // shadow from the right
-        : "inset -120px 0 0 0 var(--color-tan-200)"};
+    props.children === "About"
+      ? "inset 120px 0 0 0 var(--color-tan-200)" // shadow from the right
+      : "inset -120px 0 0 0 var(--color-tan-200)"};
 
     color: var(--color-red-500);
-  }
+  } */
 `;
 
-function Link({ route, title }) {
-  return <StyledLink to={route}>{title}</StyledLink>;
+function Link({ route, children }) {
+  return <StyledLink to={route}>{children}</StyledLink>;
 }
 
 export default Link;
--- react vite/next-level-builders/src/ui/Navbar.jsx
+++ react vite/next-level-builders/src/ui/Navbar.jsx
@@ -5,6 +5,7 @@ import Logo from "./Logo";
 import { HiOutlineBars3, HiOutlineXMark } from "react-icons/hi2";
 
 import { useState } from "react";
+import Link from "./Link";
 
 const StyledNavbar = styled.div`
   background-color: var(--color-tan-100);
@@ -13,7 +14,12 @@ const StyledNavbar = styled.div`
   justify-content: space-between;
   align-items: center;
   gap: 5px;
-  padding: 10px;
+  padding: 8px;
+`;
+
+const StyledDropDown = styled.div`
+  background-color: var(--color-tan-100);
+  height: 15vh;
 `;
 
 function Navbar() {
@@ -21,19 +27,24 @@ function Navbar() {
 
   return (
     <>
-      <StyledNavbar>
-        <Box placement="start">
+      <StyledNavbar isOpen={isOpen}>
+        <Box stack="horizontal" placement="evenly">
           <HorizontalLogo />
-        </Box>
-        <Box placement="end">
-          <Box>
-            <Logo onClick={() => setIsOpen(!isOpen)}>
-              {isOpen && <HiOutlineXMark />}
-              {!isOpen && <HiOutlineBars3 />}
-            </Logo>
-          </Box>
+          <Logo onClick={() => setIsOpen(!isOpen)}>
+            {isOpen && <HiOutlineXMark />}
+            {!isOpen && <HiOutlineBars3 />}
+          </Logo>
         </Box>
       </StyledNavbar>
+      {isOpen && (
+        <StyledDropDown>
+          <Box placement="center" stack="vertical">
+            <Link route="/">Home</Link>
+            <Link route="/about">About</Link>
+            <Link route="/projects">Projects</Link>
+          </Box>
+        </StyledDropDown>
+      )}
     </>
   );
 }
