cf1f04f719bc7bc2669e814e56b03e410297d4d9
Caleb <ritchie.caleb42@gmail.com>
2025-11-06 17:55:08 -0600

    FixedImage: Max-width 1250px, box-shadow, media query for margin & b-radius

--- netlify/src/ui/Box-ImageBackground.jsx
+++ netlify/src/ui/Box-ImageBackground.jsx
@@ -4,7 +4,6 @@ const StyledBoxImageBackground = styled.div`
   position: relative;
   text-align: center;
   width: 100%;
-  height: 1000px;
 `;
 
 function ImageBackgroundBox({ children, ...rest }) {
