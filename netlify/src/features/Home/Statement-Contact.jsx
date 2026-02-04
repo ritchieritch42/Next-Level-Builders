ba0235516dd9791bdca5a50649f7105e67ca8b88
Caleb <ritchie.caleb42@gmail.com>
2026-02-04 11:15:29 -0600

    Contact info: email/phone number

--- netlify/src/features/Home/Statement-Contact.jsx
+++ netlify/src/features/Home/Statement-Contact.jsx
@@ -1,6 +1,7 @@
 import Box from "../../ui/Box";
 import Header from "../../ui/Header";
 import Text from "../../ui/Text";
+import Card from "../../ui/Card";
 import { FaFacebook } from "react-icons/fa";
 import { BiLogoLinkedinSquare } from "react-icons/bi";
 
@@ -23,23 +24,91 @@ function ContactStatement() {
           Let's talk about how Next Level Builders can help you bring your
           vision to life.
         </Text>
-      </Box>
-      <Box stack="horizontal" gap="60px" margin="20px 0px">
-        <a
-          href="https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://www.facebook.com/61572441690192&ved=2ahUKEwjT7IiH5vOPAxWEnWoFHRUlBKwQFnoECBQQAQ&usg=AOvVaw2vkGKA9EgIa18am3LIp0EL"
-          target="_blank"
+        <Card
+          bgcolor="#ffffff"
+          colorGradient="linear-gradient(to bottom, var(--color-secondary), #4e0808)"
+          margin="5px 10px"
+          padding="30px 30px 10px 30px"
+          borderradius="5px"
+          gap="10px"
+          maxWidth="1250px"
         >
-          <FaFacebook size={50} style={{ fill: "var(--color-tan-100)" }} />
-        </a>
-        <a
-          href="https://www.linkedin.com/company/next-level-bldrs-llc/"
-          target="_blank"
-        >
-          <BiLogoLinkedinSquare
-            size={60}
-            style={{ fill: "var(--color-tan-100)" }}
-          />
-        </a>
+          <Box stack="horizontal" width="80%" placement="start">
+            <Box>
+              <Header
+                color="var(--color-tan-100)"
+                as="h1"
+                align="center"
+                shadow="0 0 3px #333333"
+                fontWeight="700"
+                font="domine"
+                textcase="upper"
+              >
+                Call
+              </Header>
+              <Text
+                color="var(--color-tan-100)"
+                align="center"
+                fontSize="1.25rem"
+              >
+                (913)-488-9750
+              </Text>
+            </Box>
+            <Box>
+              <Header
+                color="var(--color-tan-100)"
+                as="h1"
+                align="center"
+                shadow="0 0 3px #333333"
+                fontWeight="700"
+                font="domine"
+                textcase="upper"
+              >
+                Email
+              </Header>
+              <Text
+                color="var(--color-tan-100)"
+                align="center"
+                fontSize="1.25rem"
+              >
+                nextlevelbldrs@gmail.com
+              </Text>
+            </Box>
+            <Box>
+              <Header
+                color="var(--color-tan-100)"
+                as="h1"
+                align="center"
+                shadow="0 0 3px #333333"
+                fontWeight="700"
+                font="domine"
+                textcase="upper"
+              >
+                Social Media
+              </Header>
+              <Box stack="horizontal" gap="60px">
+                <a
+                  href="https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://www.facebook.com/61572441690192&ved=2ahUKEwjT7IiH5vOPAxWEnWoFHRUlBKwQFnoECBQQAQ&usg=AOvVaw2vkGKA9EgIa18am3LIp0EL"
+                  target="_blank"
+                >
+                  <FaFacebook
+                    size={50}
+                    style={{ fill: "var(--color-tan-100)" }}
+                  />
+                </a>
+                <a
+                  href="https://www.linkedin.com/company/next-level-bldrs-llc/"
+                  target="_blank"
+                >
+                  <BiLogoLinkedinSquare
+                    size={60}
+                    style={{ fill: "var(--color-tan-100)" }}
+                  />
+                </a>
+              </Box>
+            </Box>
+          </Box>
+        </Card>
       </Box>
     </>
   );
