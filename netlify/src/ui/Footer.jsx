42ae63685a356903972787e3a8a654805a58aa8d
Caleb <ritchie.caleb42@gmail.com>
2026-02-22 09:33:22 -0600

    Footer: Remove netlify hosting statement

--- netlify/src/ui/Footer.jsx
+++ netlify/src/ui/Footer.jsx
@@ -15,10 +15,6 @@ function Footer() {
           &copy; 2025 Next Level Builders, LLC
         </Text>
         <Text color="var(--color-tan-100)">All Rights Reserved</Text>
-
-        <Text alignment="center" color="var(--color-tan-100)">
-          Hosted on Netlify
-        </Text>
         <a href="https://www.netlify.com/" target="_blank">
           <SiNetlify size={30} style={{ fill: "var(--color-tan-100)" }} />
         </a>
